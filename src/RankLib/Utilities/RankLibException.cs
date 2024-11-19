ba2b991a08a743bf5fc98048a22f45fdc748af1d
Russ Cam <russ.cam@forloop.co.uk>
2024-11-19 20:32:15 +1000

    More documentation

--- src/RankLib/Utilities/RankLibException.cs
+++ src/RankLib/Utilities/RankLibException.cs
@@ -1,9 +1,12 @@
 namespace RankLib.Utilities;
 
+/// <summary>
+/// An exception thrown by this library.
+/// </summary>
 public class RankLibException : Exception
 {
-	private RankLibException(Exception e)
-		: base(e.Message, e)
+	private RankLibException(Exception innerException)
+		: base(innerException.Message, innerException)
 	{
 	}
 
@@ -12,28 +15,44 @@ public class RankLibException : Exception
 	{
 	}
 
-	private RankLibException(string message, Exception cause)
-		: base(message, cause)
+	private RankLibException(string message, Exception innerException)
+		: base(message, innerException)
 	{
 	}
 
-	public static RankLibException Create(Exception e)
+	/// <summary>
+	/// Instantiates a new instance of <see cref="RankLibException"/>, or returns
+	/// the inner exception if it is an instance of <see cref="RankLibException"/>.
+	/// </summary>
+	/// <param name="innerException">The inner exception</param>
+	/// <returns>An instance of <see cref="RankLibException"/></returns>
+	public static RankLibException Create(Exception innerException)
 	{
-		if (e is RankLibException error)
-		{
-			return error;
-		}
-		return new RankLibException(e);
+		if (innerException is RankLibException rankLibException)
+			return rankLibException;
+
+		return new RankLibException(innerException);
 	}
 
+	/// <summary>
+	/// Instantiates a new instance of <see cref="RankLibException"/> with the specified message.
+	/// </summary>
+	/// <param name="message">The exception message</param>
+	/// <returns>A new instance of <see cref="RankLibException"/></returns>
 	public static RankLibException Create(string message) => new(message);
 
-	public static RankLibException Create(string message, Exception cause)
+	/// <summary>
+	/// Instantiates a new instance of <see cref="RankLibException"/>, or returns
+	/// the inner exception if it is an instance of <see cref="RankLibException"/>.
+	/// </summary>
+	/// <param name="message">The exception message</param>
+	/// <param name="innerException">The inner exception</param>
+	/// <returns>An instance of <see cref="RankLibException"/></returns>
+	public static RankLibException Create(string message, Exception innerException)
 	{
-		if (cause is RankLibException error)
-		{
-			return error;
-		}
-		return new RankLibException(message, cause);
+		if (innerException is RankLibException rankLibException)
+			return rankLibException;
+
+		return new RankLibException(message, innerException);
 	}
 }
